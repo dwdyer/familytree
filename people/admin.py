d01b769bbfac21312a3bdaa71927ec54cc2dfc33
dwdyer <dan@uncommons.org>
2010-01-23 23:16:37 +0000

    Added marriages.

--- people/admin.py
+++ people/admin.py
@@ -1,8 +1,9 @@
-from familytree.people.models import Person
+from familytree.people.models import Person, Marriage
 from django.contrib import admin
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ['__unicode__', 'gender', 'date_of_birth']
+    list_display = ['name', 'gender', 'date_of_birth']
     list_filter = ['gender']
-
 admin.site.register(Person, PersonAdmin)
+
+admin.site.register(Marriage)
--- people/models.py
+++ people/models.py
@@ -5,25 +5,31 @@ class Person(models.Model):
     forename = models.CharField(max_length=20)
     middle_names = models.CharField(blank=True, max_length=50)
     surname = models.CharField(max_length=30)
-    maiden_name = models.CharField(blank=True, max_length=30)
+    
+    maiden_name = models.CharField(blank=True, max_length=30) # Maiden name is optional.
     gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
     date_of_birth = models.DateField()
-    date_of_death = models.DateField(blank=True, null=True)
+    date_of_death = models.DateField(blank=True, null=True) # Date of death is blank for living people.
     mother = models.ForeignKey('self', blank=True, null=True, limit_choices_to = {'gender': 'F'}, related_name='mother_of')
     father = models.ForeignKey('self', blank=True, null=True, limit_choices_to = {'gender': 'M'}, related_name='father_of')
 
-    def full_name(self):
-        '''Returns the full name of this person (with maiden name in brackets if necessary)'''
-        name = self.forename + " "
-        if self.middle_names != "":
-            name += self.middle_names + " "
-        name += self.surname
+    def name(self, use_middle_names=True, use_maiden_name=False):
+        '''Returns the full name of this person.'''
+        name = " ".join([self.forename, self.middle_names]) if use_middle_names and self.middle_names else self.forename
         if self.maiden_name != "":
-            name += u" (n\xe9e " + self.maiden_name + ")"
-        return name
+            return name + " " + (self.maiden_name if use_maiden_name else self.surname + u" (n\xe9e " + self.maiden_name + ")")
+        else:
+            return name + " " + self.surname
 
     def is_deceased(self):
         return self.date_of_death != None
+
+    def spouses(self):
+        '''Return a list of anybody that this person is or was married to.'''
+        if self.gender == 'F':
+            return [m.husband for m in Marriage.objects.filter(wife=self).order_by('wedding_date')]
+        else:
+            return [m.wife for m in Marriage.objects.filter(husband=self).order_by('wedding_date')]
 
     def siblings(self):
         '''Returns a list of this person's brothers and sisters, including half-siblings.'''
@@ -37,4 +43,15 @@ class Person(models.Model):
             return Person.objects.filter(father=self).order_by('date_of_birth')
 
     def __unicode__(self):
-        return self.full_name()
+        return self.name()
+
+
+class Marriage(models.Model):
+    husband = models.ForeignKey(Person, limit_choices_to = {'gender': 'M'}, related_name='wife_of')
+    wife = models.ForeignKey(Person, limit_choices_to = {'gender': 'F'}, related_name='husband_of')
+    wedding_date = models.DateField()
+    divorce_date = models.DateField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.husband.name(False) + ' & ' + self.wife.name(False, True)
+
