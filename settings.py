c965e2279c7e27724aac03efaa31c6e123b07253
dwdyer <dan@uncommons.org>
2010-01-25 21:09:20 +0000

    Initial CSS plus get Django to serve static files temporarily.

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'templates/media').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+import os.path
 from django.conf.urls.defaults import *
 
 from django.contrib import admin
@@ -11,4 +12,5 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     (r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.dirname(__file__), 'templates/media').replace('\\','/')}),
 )
