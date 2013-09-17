50d3f4a987b16d0e522edeb03174f01767b5c247
dwdyer <dan@uncommons.org>
2013-09-17 01:17:01 +0100

    Use MySQL by default rather than PostgreSQL.  Use localised date format.

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'familytree'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'familytree'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'familytree'         # Not used with sqlite3.
@@ -33,7 +33,11 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
