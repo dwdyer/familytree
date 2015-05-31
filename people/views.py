baf45638e65b18485f3c1b949b41cd94ae6211cc
dan <dan@uncommons.org>
2015-05-31 23:38:14 +0100

    Basic Bootstrap.

--- people/views.py
+++ people/views.py
@@ -3,19 +3,22 @@ from people.models import Person
 
 def person(request, person_id):
     person = Person.objects.get(id=person_id)
-    return render_to_response('person.html', {'person': person})
+    return render_to_response('people/person.html', {'person': person})
 
 def relatives(request, person_id):
     person = Person.objects.get(id=person_id)
     title = 'Blood Relatives of ' + person.name()
-    return render_to_response('relatives.html', {'title': title, 'relatives': person.relatives()})
+    return render_to_response('people/relatives.html',
+                              {'title': title, 'relatives': person.relatives()})
 
 def descendants(request, person_id):
     person = Person.objects.get(id=person_id)
     title = 'Descendants of ' + person.name()
-    return render_to_response('relatives.html', {'title': title, 'relatives': person.descendants()})
+    return render_to_response('people/relatives.html',
+                              {'title': title, 'relatives': person.descendants()})
 
 def ancestors(request, person_id):
     person = Person.objects.get(id=person_id)
     title = 'Ancestors of ' + person.name()
-    return render_to_response('relatives.html', {'title': title, 'relatives': person.ancestors()})
+    return render_to_response('people/relatives.html',
+                              {'title': title, 'relatives': person.ancestors()})
