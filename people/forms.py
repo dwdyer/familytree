ed9a5e56bdecafefae9e1213793fe3c495c719d0
dan <dan@uncommons.org>
2016-12-18 17:35:18 +0000

    Updated to select2 4.0. Use select2 for parent selection.

--- people/forms.py
+++ people/forms.py
@@ -13,6 +13,16 @@ class BootstrapModelForm(forms.ModelForm):
                     field.widget.attrs['placeholder'] = field.help_text
 
 
+class PersonChoiceField(forms.ModelChoiceField):
+    '''Custom version of the choice field that formats the user and their birth
+    year in a way that can be interpreted by the client-side JavaScript.'''
+    def label_from_instance(self, obj):
+        if obj.date_of_birth():
+            return '{0}|{1}'.format(obj.name(), obj.date_of_birth().year)
+        else:
+            return obj.name()
+
+
 class AddPersonForm(BootstrapModelForm):
     
     class Meta:
@@ -20,4 +30,6 @@ class AddPersonForm(BootstrapModelForm):
         fields = ['gender', 'deceased', 'blood_relative',
                   'forename', 'middle_names', 'known_as', 'surname', 'maiden_name',
                   'mother', 'father', 'notes']
+        field_classes = {'mother': PersonChoiceField,
+                         'father': PersonChoiceField}
         widgets = {'gender': forms.RadioSelect}
