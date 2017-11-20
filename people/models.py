123f0c57831e2f398de50ec9e0de3bae9d747c24
dan <dan@uncommons.org>
2017-11-20 23:40:46 +0000

    Avoid destructive cascade when removing birth/death event.

--- people/models.py
+++ people/models.py
@@ -85,8 +85,8 @@ class Person(models.Model):
                               choices=(('M', 'Male'), ('F', 'Female')),
                               blank=False,
                               default=None)
-    birth = models.ForeignKey('Event', null=True, blank=True, related_name='+')
-    death = models.ForeignKey('Event', null=True, blank=True, related_name='+')
+    birth = models.ForeignKey('Event', models.SET_NULL, null=True, blank=True, related_name='+')
+    death = models.ForeignKey('Event', models.SET_NULL, null=True, blank=True, related_name='+')
     deceased = models.BooleanField(default=True)
     blood_relative = models.BooleanField(default=True)
     mother = models.ForeignKey('self',
