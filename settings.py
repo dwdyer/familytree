4396371ac1dc8a475e52b3827b1b98d4d601ea1f
dan <dan@uncommons.org>
2018-04-02 15:03:23 +0100

    Formatting for number of people.

--- settings.py
+++ settings.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.humanize',
     'tinymce',
     'taggit',
     'easy_thumbnails',
