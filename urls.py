1c76bc25639c6a9fde384862d2ff047809b884b1
Daniel Dyer <dan@uncommons.org>
2025-11-08 15:55:14 +0000

    Fix for incompatible dependencies. Fixes for Django 4.x.

--- urls.py
+++ urls.py
@@ -1,21 +1,21 @@
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 import django.views.static
 import os.path
 import settings
 
 urlpatterns = [
-    url(r'', include('people.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
-    url(r'^logout/$', LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('people.urls')),
+    path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 # Get Django to serve media files in debug mode.
 if settings.DEBUG:
-    urlpatterns += [url(r'^media/(?P<path>.*)$',
-                        django.views.static.serve,
-                        {'document_root': settings.MEDIA_ROOT})]
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$',
+                            django.views.static.serve,
+                            {'document_root': settings.MEDIA_ROOT})]
